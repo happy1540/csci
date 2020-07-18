39e5ed5498c3af83b083c97b3a4d4554cb66eea1
happy904 <happybaljot1540@gmai.com>
2020-07-19 05:01:36 +0530

    Update first-code.js

--- lab14/first-code.js
+++ lab14/first-code.js
@@ -1,11 +1,10 @@
 say_hello= function(){
 	count = 54;
 	
-	count = count* 2;
+	count = count * 2;
 	
 	lenght = 7.32;
 	hello = "hello";
 	msg = hello;
-	alert(msg + ' world ' + count);
-	
+	alert(msg + ' '+ ' world ' + count);
 }
