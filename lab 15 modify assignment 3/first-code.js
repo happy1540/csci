7361a0d338e67526b04169d363481f50db92f09f
happy904 <happybaljot1540@gmai.com>
2020-08-16 09:27:26 +0530

    u

--- lab 15 modify assignment 3/first-code.js
+++ lab 15 modify assignment 3/first-code.js
@@ -1,70 +1,88 @@
-add = function() {
-    result = parseInt(jQuery('#operand1').val()) + parseInt(jQuery('#operand2').val());
-    text = jQuery('#operand1').val() +" + "+ jQuery('#operand2').val() + " = "
-    result = text + result;
-    jQuery('#result').text(result)
-    
-    old_div_content = jQuery('#history').html();
-    result = "<span id='add_style'>" + result + "</span>";
-    new_div_content = old_div_content + result + '<br><br>';
-    jQuery ('#history').html(new_div_content);
-}
+add = function(){
+result = parseInt(jQuery( '#op1').val()) + parseInt(jQuery('#op2').val());
+  // jQuery ('operand_1').html ('1232132123');
+text = jQuery( '#op1').val() + " + " + jQuery('#op2').val() + " = " ;
 
-minus = function() {
-    result = parseInt(jQuery('#operand1').val()) - parseInt(jQuery('#operand2').val());
-    text = jQuery('#operand1').val() +" - "+ jQuery('#operand2').val() + " = "
-    
+jQuery('#result').val(text + result);
 
-    result = text + result;
-    jQuery('#result').html(result)
-    
-    old_div_content = jQuery('#history').html();
-    result = "<span id='min_style'>" + result + "</span>";
-    new_div_content = old_div_content + result + '<br><br>';
-    jQuery ('#history').html(new_div_content);
-}
+styled_result = "<span id = 'addstyle'>" + text + result + "</span>";
+// old_div_content =jQuery('#history').html();
+// new_div_content = old_div_content + styled_result +'<br>';
+jQuery ('#result').html(text + result ); // or use
+// jQuery ('#history').html(new_div_content);
+jQuery ('#history').append(styled_result +'<br>');
+// or use
+                                           // jQuery ('#history'). append (styled_result+ 'br'):
+                                          // append will add the new content without overwriting
+                                          // no need for line 10 and 11in this case!
+                                        }
+sub = function(){
+  result = parseInt(jQuery( '#op1').val()) - parseInt(jQuery('#op2').val());
 
-multiply = function() {
-    result = parseInt(jQuery('#operand1').val()) * parseInt(jQuery('#operand2').val());
-    text = jQuery('#operand1').val() +" * "+ jQuery('#operand2').val() + " = "
-    result = text + result;
-    jQuery('#result').html(result)
-    
-    old_div_content = jQuery('#history').html();
-    result = "<span id='mul_style'>" + result + "</span>";
-    new_div_content = old_div_content + result + '<br><br>';
-    jQuery ('#history').html(new_div_content);
-}
+text = jQuery( '#op1').val() + " - " + jQuery('#op2').val() + " = " ;
 
+jQuery('#result').val(text + result);
 
-divide = function() {
-    result = parseInt(jQuery('#operand1').val()) / parseInt(jQuery('#operand2').val());
-    text = jQuery('#operand1').val() +" / "+ jQuery('#operand2').val() + " = "
-    result = text + result;
-    jQuery('#result').html(result)
-    
-    old_div_content = jQuery('#history').html();
-    result = "<span id='div_style'>" + result + "</span>";
-    new_div_content = old_div_content + result + '<br><br>';
-    jQuery ('#history').html(new_div_content);
-}
-   
-    show = function() {
-        jQuery('#history').show()
-    }
-
-    hide = function(){
-        jQuery('#history').hide()
-    } 
-
-    setup = function(){
-        jQuery('#addbutton').click(add);
-        jQuery('#subbutton').click(minus);
-        jQuery('#mulbutton').click(multiply);
-        jQuery('#divbutton').click(divide);
-        jQuery('#show_button').click(show);
-        jQuery('#hide_button').click(hide);
+styled_result = "<span id = 'substyle'>" + text + result + "</span>";
+old_div_content =jQuery('#history').html();
+new_div_content = old_div_content + styled_result +'<br>';
+jQuery ('#result').html(text + result );
+jQuery ('#history').html(new_div_content);
     }
 
 
-    jQuery(document).ready(setup)
+div= function(){
+result = parseInt(jQuery( '#op1').val()) / parseInt(jQuery('#op2').val());
+
+text = jQuery( '#op1').val() + " / " + jQuery('#op2').val() + " = " ;
+
+jQuery('#result').val(text + result);
+
+styled_result = "<span id = 'divstyle'>" + text + result + "</span>";
+old_div_content =jQuery('#history').html();
+new_div_content = old_div_content + styled_result +'<br>';
+jQuery ('#result').html(text + result);
+jQuery ('#history').html(new_div_content);
+    }
+
+mul = function(){
+result = parseInt(jQuery( '#op1').val()) * parseInt(jQuery('#op2').val());
+
+text = jQuery( '#op1').val() + " * "+ jQuery('#op2').val() + " = " ;
+jQuery('#result').val(text + result);
+
+styled_result = "<span id = 'mulstyle'>" + text + result + "</span>";
+old_div_content =jQuery('#history').html();
+new_div_content = old_div_content + styled_result +'<br>';
+jQuery ('#result').html(text + result);
+jQuery ('#history').html(new_div_content);
+}
+show= function (){
+// jQuery('#history').show();}
+jQuery('#history').attr('style', 'display:block');
+}
+hide= function(){
+// jQuery('#history').hide(); // or use  // jQuery ('#history'). attr ('style', 'display:none');
+// jQuery('#history').attr('style', 'display:none');
+jQuery('#history').css('display', 'none');
+}
+
+clear= function(){
+  jQuery('#history').text(' ');
+}
+
+
+setup = function(){
+jQuery('#add_button').click(add);
+ jQuery('#sub_button').click(sub);
+ jQuery('#mul_button').click(mul);
+ jQuery('#div_button').click(div);
+ jQuery('#add_button').mouseenter(add);
+  jQuery('#sub_button').mouseenter(sub);
+  jQuery('#mul_button').mouseenter(mul);
+  jQuery('#div_button').mouseenter(div);
+ jQuery('#show_button').click(show);
+ jQuery('#hide_button').click(hide);
+ jQuery('#clearbutton').click(clear);
+}
+jQuery(document).ready(setup);
