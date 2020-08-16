f5f8c5172d65892a2b6e7134e66e4b68dfd84129
happy904 <happybaljot1540@gmai.com>
2020-08-16 08:35:05 +0530

    u

--- lab 15 modify assignment 3/first-code.js
+++ lab 15 modify assignment 3/first-code.js
@@ -1,16 +1,70 @@
-    #addStyle {
-      background-color: Tomato;
+add = function() {
+    result = parseInt(jQuery('#operand1').val()) + parseInt(jQuery('#operand2').val());
+    text = jQuery('#operand1').val() +" + "+ jQuery('#operand2').val() + " = "
+    result = text + result;
+    jQuery('#result').text(result)
+    
+    old_div_content = jQuery('#history').html();
+    result = "<span id='add_style'>" + result + "</span>";
+    new_div_content = old_div_content + result + '<br><br>';
+    jQuery ('#history').html(new_div_content);
+}
+
+minus = function() {
+    result = parseInt(jQuery('#operand1').val()) - parseInt(jQuery('#operand2').val());
+    text = jQuery('#operand1').val() +" - "+ jQuery('#operand2').val() + " = "
+    
+
+    result = text + result;
+    jQuery('#result').html(result)
+    
+    old_div_content = jQuery('#history').html();
+    result = "<span id='min_style'>" + result + "</span>";
+    new_div_content = old_div_content + result + '<br><br>';
+    jQuery ('#history').html(new_div_content);
+}
+
+multiply = function() {
+    result = parseInt(jQuery('#operand1').val()) * parseInt(jQuery('#operand2').val());
+    text = jQuery('#operand1').val() +" * "+ jQuery('#operand2').val() + " = "
+    result = text + result;
+    jQuery('#result').html(result)
+    
+    old_div_content = jQuery('#history').html();
+    result = "<span id='mul_style'>" + result + "</span>";
+    new_div_content = old_div_content + result + '<br><br>';
+    jQuery ('#history').html(new_div_content);
+}
+
+
+divide = function() {
+    result = parseInt(jQuery('#operand1').val()) / parseInt(jQuery('#operand2').val());
+    text = jQuery('#operand1').val() +" / "+ jQuery('#operand2').val() + " = "
+    result = text + result;
+    jQuery('#result').html(result)
+    
+    old_div_content = jQuery('#history').html();
+    result = "<span id='div_style'>" + result + "</span>";
+    new_div_content = old_div_content + result + '<br><br>';
+    jQuery ('#history').html(new_div_content);
+}
+   
+    show = function() {
+        jQuery('#history').show()
     }
 
-    #subStyle {
-      background-color: DodgerBlue;
+    hide = function(){
+        jQuery('#history').hide()
+    } 
+
+    setup = function(){
+        jQuery('#addbutton').click(add);
+        jQuery('#subbutton').click(minus);
+        jQuery('#mulbutton').click(multiply);
+        jQuery('#divbutton').click(divide);
+        jQuery('#show_button').click(show);
+        jQuery('#hide_button').click(hide);
     }
 
-    #mulStyle {
-      background-color: SlateBlue;
-    }
 
-    #divStyle {
-      background-color: MediumSeaGreen;
-    }
-  
+    jQuery(document).ready(setup)
